299f5749d3dff8b6c990f640f42bd3fb351c29ea
emousse <etienne.mousse@gmail.com>
2016-11-14 17:57:52 +0100

    add navigation drawer + navigation view

--- app/src/main/java/com/oc/emousse/multilingua/MainActivity.java
+++ app/src/main/java/com/oc/emousse/multilingua/MainActivity.java
@@ -1,11 +1,21 @@
 package com.oc.emousse.multilingua;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.oc.emousse.multilingua.pref.UserShared;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,37 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing toolbar
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        //Initializing navigation view
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        //Set drawer layout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setupDrawer();
+
+
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
     }
 }

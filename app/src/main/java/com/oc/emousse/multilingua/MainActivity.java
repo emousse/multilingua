390a30e798355ef3c6a75987bdc9349f74f5c8cc
emousse <etienne.mousse@gmail.com>
2016-11-14 18:30:29 +0100

    navigationViewListener

--- app/src/main/java/com/oc/emousse/multilingua/MainActivity.java
+++ app/src/main/java/com/oc/emousse/multilingua/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.oc.emousse.multilingua.pref.UserShared;
 
@@ -28,14 +30,54 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        //Initializing navigation view
+        //Initializing navigation view and setChecked first item
         navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationViewListener();
+        navigationView.getMenu().getItem(0).setChecked(true);
 
         //Set drawer layout
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         setupDrawer();
 
 
+    }
+
+    private void navigationViewListener(){
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+                    // For rest of the options we just show a toast on click
+
+                    case R.id.lessons:
+                        Toast.makeText(getApplicationContext(),"Cours",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.quiz:
+                        Toast.makeText(getApplicationContext(),"Quiz",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(),"Logout",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
     }
 
     private void setupDrawer() {
